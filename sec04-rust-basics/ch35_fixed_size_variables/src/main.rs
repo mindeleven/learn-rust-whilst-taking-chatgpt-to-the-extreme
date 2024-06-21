01a68cce8eb51c6ac2cd38652787fac80c4f258f
mindeleven <mindeleven@posteo.de>
2024-06-21 10:59:21 +0200

    New project to code along with section 04/35, fixed sized variables

---
+++ sec04-rust-basics/ch35_fixed_size_variables/src/main.rs
@@ -0,0 +1,15 @@
+const OUR_COURSE: &str = "Rust with AutoGPT";
+
+fn main() {
+
+    println!("Welcome to our course \"{}\"", OUR_COURSE);
+    
+    // stack
+    let x: i32;
+    x = 2;
+    println!("x is {}", x);
+
+    let y = 4;
+    println!("y is {}", y);
+
+}
