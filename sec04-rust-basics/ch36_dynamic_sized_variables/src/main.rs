457814ff785b00f315842ae9ae9144a557e9c2c6
mindeleven <mindeleven@posteo.de>
2024-06-23 13:36:42 +0200

    Small exercises with strings and vectors

--- sec04-rust-basics/ch36_dynamic_sized_variables/src/main.rs
+++ sec04-rust-basics/ch36_dynamic_sized_variables/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    // string literal == reference to a string
+    let name: &str = "Fernand";
+    println!("The name is {}", name);
+
+    // dynamic string on the heao
+    let dynamic_name = String::from("Fernand Braudel");
+    // printing out with a pointer
+    println!("The dynamic name ({}) in memory is {:p}", dynamic_name, &dynamic_name); // 0x16d08eb18
+    
+    let make_name_dynamic = name.to_string();
+    println!("make_name_dynamic is {}", make_name_dynamic);
+    
+    // string slice of dynamic_string
+    let str_slice = &dynamic_name[0..5];
+    println!("str_slice of dynamic_name is {}", str_slice);
+    
+    // mutable vactor
+    let mut chars: Vec<char> = Vec::new();
+    chars.insert(0, 'h'); // inserting a value with index
+    chars.insert(1, 'e');
+    chars.insert(2, 'l');
+    chars.push('l'); // adding an element at the end
+    chars.push('o');
+    println!("{:?}", chars); // printing out chars
+    dbg!(chars); // printing out in debug mode
+
 }
