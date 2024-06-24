ccbeb5a3f38cb1407d0df9a0b13d1ea3d38c3970
mindeleven <mindeleven@posteo.de>
2024-06-24 12:39:15 +0200

    Examples for number literals

--- sec04-rust-basics/ch39_num_literals_raw_stings/src/main.rs
+++ sec04-rust-basics/ch39_num_literals_raw_stings/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // number literals (taken from the Rust book)
+    println!("Big number is {}", 98_220_000); // underscores for large numbers
+    println!("Hex is {}", 0xff); // hexadecimal number
+    println!("Octal is {}", 0o77); // octal number
+    println!("Binary is {}", 0b1111_0000); // binary
+    println!("Bites 'A' is {}", b'A'); // binary
 }
