6b537dd4a140ecd9ab05fb3616f8f054e4d07f6f
mindeleven <mindeleven@posteo.de>
2024-06-25 12:29:32 +0200

    Repeating the last two examples with build in Result and Option types

--- sec05-intermediate-rust/ch43_enums_and_structs/src/m1_enums.rs
+++ sec05-intermediate-rust/ch43_enums_and_structs/src/m1_enums.rs
@@ -35,12 +35,33 @@ fn check_under_five(num: u8) -> GivenResult<u8, String> {
     }
 }
 
+// same like above with build in Result
+fn check_under_five_build_in(num: u8) -> Result<u8, String> {
+    if num < 5 {
+        Result::Ok(num)
+    } else {
+        Result::Err(String::from("Not under 5!!!"))
+    }
+}
+
+
 fn remainder_zero(num: f32) -> GivenOption<f32> {
     let remainder_check = num % 10.0;
     if remainder_check != 0.0 {
         GivenOption::Some(remainder_check)
     } else {
         GivenOption::None
+    }
+
+}
+
+// same like above with build in Option
+fn remainder_zero_build_in(num: f32) -> Option<f32> {
+    let remainder_check = num % 10.0;
+    if remainder_check != 0.0 {
+        Option::Some(remainder_check)
+    } else {
+        Option::None
     }
 
 }
@@ -61,11 +82,25 @@ mod test {
         let under_five_res = check_under_five(7);
         dbg!(under_five_res);
 
+        let under_five_res_bi = check_under_five_build_in(2);
+        dbg!(under_five_res_bi.unwrap());
+        
+        /* let under_five_res_bi = check_under_five_build_in(7);
+        // thread 'm1_enums::test::tests_enums' panicked at src/m1_enums.rs:78:32:
+        // called `Result::unwrap()` on an `Err` value: "Not under 5!!!"
+        dbg!(under_five_res_bi.unwrap()); */
+
         let remainder = remainder_zero(12.2);
         dbg!(remainder);
 
         let remainder = remainder_zero(100.0);
         dbg!(remainder);
+
+        let remainder_build_in = remainder_zero_build_in(12.2);
+        dbg!(remainder_build_in);
+
+        let remainder_build_in = remainder_zero_build_in(100.0);
+        dbg!(remainder_build_in);
     }
 
 }
