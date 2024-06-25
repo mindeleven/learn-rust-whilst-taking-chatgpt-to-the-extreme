5e0caccb9c1895efd27ec9c34ed30dfb132336a5
mindeleven <mindeleven@posteo.de>
2024-06-25 10:55:04 +0200

    A first enum

--- sec05-intermediate-rust/ch43_enums_and_structs/src/m1_enums.rs
+++ sec05-intermediate-rust/ch43_enums_and_structs/src/m1_enums.rs
@@ -1,14 +1,28 @@
+#![allow(dead_code, unused_imports)]
 
+#[derive(Debug)]
+enum CarColor {
+    Red,
+    Green,
+    Blue,
+    Silver
+}
+
+fn create_car_color_blue() -> CarColor {
+    let my_car_color = CarColor::Blue;
+    my_car_color
+}
 
 
 #[cfg(test)]
 mod test {
     use super::*;
-
+    
+    // cargo test tests_enums -- --nocapture
     #[test] 
     fn tests_enums() {
-        // cargo test tests_enums -- --nocapture
-        dbg!("Hi there!");
+        let my_car_color = create_car_color_blue();
+        dbg!(my_car_color);
     }
 
 }
