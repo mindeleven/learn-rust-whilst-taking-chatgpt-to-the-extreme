902e0cd1fc9cc5330a98977e849d1368ca840c3e
mindeleven <mindeleven@posteo.de>
2024-06-25 11:21:46 +0200

    Custom result enum with generic type

--- sec05-intermediate-rust/ch43_enums_and_structs/src/m1_enums.rs
+++ sec05-intermediate-rust/ch43_enums_and_structs/src/m1_enums.rs
@@ -8,11 +8,25 @@ enum CarColor {
     Silver
 }
 
+// result enum with generic type
+#[derive(Debug)]
+enum GivenResult<T, E> {
+    Ok(T),
+    Err(E)
+}
+
 fn create_car_color_blue() -> CarColor {
     let my_car_color = CarColor::Blue;
     my_car_color
 }
 
+fn check_under_five(num: u8) -> GivenResult<u8, String> {
+    if num < 5 {
+        GivenResult::Ok(num)
+    } else {
+        GivenResult::Err(String::from("Not under 5!!!"))
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -23,6 +37,12 @@ mod test {
     fn tests_enums() {
         let my_car_color = create_car_color_blue();
         dbg!(my_car_color);
+
+        let under_five_res = check_under_five(2);
+        dbg!(under_five_res);
+
+        let under_five_res = check_under_five(7);
+        dbg!(under_five_res);
     }
 
 }
