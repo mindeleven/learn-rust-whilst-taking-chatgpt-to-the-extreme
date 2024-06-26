7dd7999ffaa3d23e4ef75d746ee9d1120e3fede7
mindeleven <mindeleven@posteo.de>
2024-06-26 12:48:54 +0200

    Scaffolding structure to work along with the traits part of the course

---
+++ sec05-intermediate-rust/ch43_enums_and_structs/src/m3_traits.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code, unused_imports)]
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    // cargo test tests_traits -- --nocapture
+    #[test] 
+    fn tests_traits() {
+
+        dbg!("tests_traits");
+    
+    }
+
+}
+
--- sec05-intermediate-rust/ch43_enums_and_structs/src/main.rs
+++ sec05-intermediate-rust/ch43_enums_and_structs/src/main.rs
@@ -1,5 +1,6 @@
 mod m1_enums;
 mod m2_structs;
+mod m3_traits;
 
 fn main() {
     println!("Sub-project for coding along with the enums and structs part.");
