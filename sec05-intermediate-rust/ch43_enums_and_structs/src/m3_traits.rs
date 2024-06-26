bc9d30fa2efbc1f564f1014c0db6fa67bf7e3555
mindeleven <mindeleven@posteo.de>
2024-06-26 13:24:31 +0200

    Trait example with implementation for enum

--- sec05-intermediate-rust/ch43_enums_and_structs/src/m3_traits.rs
+++ sec05-intermediate-rust/ch43_enums_and_structs/src/m3_traits.rs
@@ -1,5 +1,28 @@
 #![allow(dead_code, unused_imports)]
 
+/// traits allow to do an interface definition
+/// they allow to define a shared interface for methods 
+/// that different types need to implement
+trait Attacker {
+    fn choose_style(&self) -> String;
+}
+
+enum Character {
+    Warrior,
+    Archer,
+    Wizard
+}
+
+impl Attacker for Character {
+    fn choose_style(&self) -> String {
+        match self {
+            Character::Warrior => "wing chun".to_string(),
+            Character::Archer => "kung fu".to_string(),
+            Character::Wizard => "tai chi".to_string(),
+        }   
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -9,7 +32,9 @@ mod test {
     #[test] 
     fn tests_traits() {
 
-        dbg!("tests_traits");
+        let my_character: Character = Character::Archer;
+        let chosen_fighting_style = my_character.choose_style();
+        dbg!(chosen_fighting_style);
     
     }
 
