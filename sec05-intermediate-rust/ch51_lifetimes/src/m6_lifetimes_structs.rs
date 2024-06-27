6a54d770a3f325bca493f6ef36dbd665cd540ceb
mindeleven <mindeleven@posteo.de>
2024-06-27 15:07:18 +0200

    Lifetime example with a struct

--- sec05-intermediate-rust/ch51_lifetimes/src/m6_lifetimes_structs.rs
+++ sec05-intermediate-rust/ch51_lifetimes/src/m6_lifetimes_structs.rs
@@ -5,7 +5,38 @@
 /// struct needs lifetime specifier 
 struct Person<'p> { 
     name: &'p str,
-    points: &'p i32, // w/o lifetime: ´missing lifetime specifier´ error
+    points: &'p f32, // w/o lifetime: ´missing lifetime specifier´ error
+}
+
+// function example with generics
+fn example_5_with_struct() {
+
+    let highest_points: &f32;
+    let new_value: f32;
+
+    let alice = Person { name: "alice", points: &50.3 };
+    
+    {
+        let bob = Person { name: "bob", points: &40.2 };
+        highest_points = largest::<f32>(alice.points, bob.points);
+        new_value = *highest_points;
+    }
+
+    println!("highest_points is {}", new_value);
+    
+    // specifying type T generic <'a, T>
+    // without further specification we get an error: 
+    // -> binary operation `>` cannot be applied to type `&T`
+    // consider restricting type parameter `T`: `: std::cmp::PartialOrd`
+    // type T needs to be a type the implements PartialOrd
+    fn largest<'a, T: PartialOrd>(compare_1: &'a T, compare_2: &'a T) -> &'a T {
+        if compare_1 > compare_2 {
+            compare_1 
+        } else {
+            compare_2
+        }
+    }
+
 }
 
 // function example with generics
@@ -31,6 +62,21 @@ fn example_4_with_generics() {
         } else {
             compare_2
         }
+    }
+
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    // cargo test tests_lifetimes_structs -- --nocapture
+    #[test] 
+    fn tests_lifetimes_structs() {
+
+        dbg!("tests_lifetimes_structs
+");
+    
     }
 
 }
