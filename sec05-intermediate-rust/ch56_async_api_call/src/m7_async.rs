96b264293610ecc0f5b5546d6c4ca83936d9d47d
mindeleven <mindeleven@posteo.de>
2024-07-01 14:33:53 +0200

    Example for a simple API call

--- sec05-intermediate-rust/ch56_async_api_call/src/m7_async.rs
+++ sec05-intermediate-rust/ch56_async_api_call/src/m7_async.rs
@@ -1,5 +1,17 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 
+use ethers::types::transaction::response;
+
+// function returns json formated structure or error from reqwest
+async fn my_asnc_call(url: &str) -> Result<serde_json::Value, reqwest::Error> {
+    
+    let response: serde_json::Value = reqwest::get(url)
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+
+    Ok(response)
+}
 
 #[cfg(test)]
 mod test {
@@ -8,8 +20,16 @@ mod test {
     // cargo test tests_calls_asnyc_fn -- --nocapture
     #[tokio::test] 
     async fn tests_calls_asnyc_fn() {
-    
-        dbg!("tests_calls_asnyc_fn");
+
+        let api_url = "https://cat-fact.herokuapp.com/facts/";
+        // async call to api endpoint
+        let my_res: Result<serde_json::Value, reqwest::Error> = my_asnc_call(api_url)
+            .await; // needs to be awaited
+        // let's see what we got 
+        match my_res {
+            Ok(value) => { dbg!(value); },
+            Err(_) => { panic!("Api call failed!"); },
+        };
     
     }
 
