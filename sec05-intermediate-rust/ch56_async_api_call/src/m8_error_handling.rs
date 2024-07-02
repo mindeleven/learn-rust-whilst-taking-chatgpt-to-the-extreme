fcac7baebf7970a30f43684b83414a6b07c46341
mindeleven <mindeleven@posteo.de>
2024-07-02 10:08:47 +0200

    Example for converting an Error type that gets returned from a function

--- sec05-intermediate-rust/ch56_async_api_call/src/m8_error_handling.rs
+++ sec05-intermediate-rust/ch56_async_api_call/src/m8_error_handling.rs
@@ -1,14 +1,39 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 
+use std::io::{
+    Error,
+    ErrorKind
+};
+
 // function returns json formated structure or error from reqwest
-async fn my_asnc_call(url: &str) -> Result<serde_json::Value, reqwest::Error> {
+async fn my_asnc_call(url: &str) -> Result<serde_json::Value, Error> {
     
+    /* 
     let response: serde_json::Value = reqwest::get(url)
         .await?
         .json::<serde_json::Value>()
-        .await?;
+        .await?; 
+    */
+    let response = reqwest::get(url)
+       .await
+       .map_err(|_| {
+            Error::new(
+              ErrorKind::Other,
+              "Could not retrieve response"
+            )
+       })?; // question mark here is going to unwrap or return an error in the io::Error format
 
-    Ok(response)
+    let json_response = response
+        .json::<serde_json::Value>()
+        .await
+        .map_err(|_| {
+            Error::new(
+            ErrorKind::Other,
+            "Could not decode to JSON"
+            )
+    })?;
+
+    Ok(json_response)
 }
 
 #[cfg(test)]
@@ -21,7 +46,7 @@ mod test {
 
         let api_url = "https://cat-fact.herokuapp.com/facts/";
         // async call to api endpoint
-        let my_res: Result<serde_json::Value, reqwest::Error> = my_asnc_call(api_url)
+        let my_res: Result<serde_json::Value, std::io::Error> = my_asnc_call(api_url)
             .await; // needs to be awaited
         // let's see what we got 
         match my_res {
