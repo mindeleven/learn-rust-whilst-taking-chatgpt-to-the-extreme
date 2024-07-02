ef841fd74a078bdb3a5b3fc978bc79c032c6e0d8
mindeleven <mindeleven@posteo.de>
2024-07-02 10:48:34 +0200

    Defining structs to structure the data needed in the app

--- sec07-web-server-template/ch74_web_template/src/main.rs
+++ sec07-web-server-template/ch74_web_template/src/main.rs
@@ -31,6 +31,28 @@ use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
 
+/// server is going to represent simple to do app
+/// structuring data we're going to store in a json file
+/// storing task data
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool,
+}
+/// storing user data
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String, // easy storing as a string here
+}
+/// database
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>,
+}
 
 fn main() {
     println!("Hello, world!");
