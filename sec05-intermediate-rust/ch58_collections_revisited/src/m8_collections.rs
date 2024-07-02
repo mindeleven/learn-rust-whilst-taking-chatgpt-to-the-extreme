8b05e3e3721d715a7d52631fb1e4a74d181ff646
mindeleven <mindeleven@posteo.de>
2024-07-02 13:51:11 +0200

    Simple HashMap example

--- sec05-intermediate-rust/ch58_collections_revisited/src/m8_collections.rs
+++ sec05-intermediate-rust/ch58_collections_revisited/src/m8_collections.rs
@@ -12,12 +12,21 @@ mod test {
     // cargo test tests_hashmap -- --nocapture
     #[test] 
     fn tests_hashmap() {
-        dbg!("tests_hashmap");
+        // hashmaps store key-value pairs
+        let mut results_hm: HashMap<&str, u32> = HashMap::new();
+        let person_1 = "Alice";
+        let person_2 = "Bob";
+        results_hm.insert(&person_1, 55);
+        results_hm.insert(&person_2, 52);
+        // hashmap.get returns an option
+        let test_result = results_hm.get(person_1);
+        dbg!(test_result);
     }
 
     // cargo test tests_hashset -- --nocapture
     #[test] 
     fn tests_hashset() {
+        // using a hashset to store values in general
         dbg!("tests_hashset");
     }
 }
