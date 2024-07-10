5fbc02937de2f4164929c4adc49e221cac5ac895
mindeleven <mindeleven@posteo.de>
2024-07-10 09:23:20 +0200

    Importing GPT API keys, using get_user_response() to prompt the user for an input

--- sec08-auto-gpt-project/ch86_auto_gippity/src/main.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/main.rs
@@ -1,10 +1,30 @@
+/// GPT Chat Completions API
+/// https://platform.openai.com/docs/guides/text-generation/chat-completions-api
+
 mod ai_functions;
 mod apis;
 mod helpers;
 mod models;
 
 use helpers::command_line::get_user_response;
+use std::fs;
 
 fn main() {
-    println!("Hello, auto gippity!");
+    let api_key = fs::read_to_string(
+        "../../../../../../tmp/chat_gpt/gippity_key_1.txt"
+    ).expect("Unable to read file");
+
+    let org_id = fs::read_to_string(
+        "../../../../../../tmp/chat_gpt/org_id.txt"
+    ).expect("Unable to read file");
+
+    dbg!(api_key);
+    dbg!(org_id);
+
+    let user_req = get_user_response(
+        "What webserver are we building today?"
+    );
+    dbg!(user_req);
+
+    // next thing to do: pass the request to our agent
 }
