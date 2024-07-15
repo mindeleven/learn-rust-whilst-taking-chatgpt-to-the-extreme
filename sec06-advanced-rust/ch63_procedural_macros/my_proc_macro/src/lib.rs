f47563bef87b21196cdc460af54974e01d4c954f
mindeleven <mindeleven@posteo.de>
2024-07-15 10:46:40 +0200

    Writing out code for a procedural macro that takes a function and creates a function from it

--- sec06-advanced-rust/ch63_procedural_macros/my_proc_macro/src/lib.rs
+++ sec06-advanced-rust/ch63_procedural_macros/my_proc_macro/src/lib.rs
@@ -1,1 +1,41 @@
 /// we need to export our procedural macro here
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::{
+    quote, 
+    ToTokens
+};
+use syn::{
+    parse_macro_input,
+    ItemFn
+};
+
+// a function that makes a function
+#[proc_macro_attribute]
+pub fn function_to_string(item: TokenStream, _attr: TokenStream) -> TokenStream {
+    
+    // parse the input function
+    let  input_fn: ItemFn = parse_macro_input!(item as ItemFn);
+
+    // getting it into a formated string with the format macro
+    // create string representation of function
+    let function_str: String = format!("{}", input_fn.to_token_stream());
+
+    // define a new function with the same signature as the input function
+    let fn_ident: proc_macro2::Ident = input_fn.sig.ident;
+    let fn_inputs: syn::punctuated::Punctuated<syn::FnArg, syn::token::Comma> 
+        = input_fn.sig.inputs;
+    let fn_generics: syn::Generics = input_fn.sig.generics;
+
+    // constructing the output
+    // generate output function
+    let output: proc_macro2::TokenStream = quote! {
+        pub fn #fn_ident #fn_generics(#fn_inputs) -> &'static str {
+            #function_str
+        }
+    };
+
+    output.into()
+
+}
