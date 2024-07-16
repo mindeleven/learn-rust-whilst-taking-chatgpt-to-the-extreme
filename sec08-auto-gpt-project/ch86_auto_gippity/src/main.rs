1fa3ee4c75edad9e164b3e9b7f85a073f9773d30
mindeleven <mindeleven@posteo.de>
2024-07-16 11:42:50 +0200

    Added comment

--- sec08-auto-gpt-project/ch86_auto_gippity/src/main.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/main.rs
@@ -9,7 +9,7 @@ mod models;
 use helpers::command_line::get_user_response;
 
 fn main() {
-    let user_req = get_user_response(
+    let user_req = get_user_response( // helpers::command_line::get_user_response
         "What webserver are we building today?"
     );
     dbg!(user_req);
