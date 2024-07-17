d7ab3d11b620ddaf224c3a76f52bb3ff68a0dd3f
mindeleven <mindeleven@posteo.de>
2024-07-17 13:17:35 +0200

    Beginning to write out a test for the extend_ai_function helper function

--- sec08-auto-gpt-project/ch86_auto_gippity/src/helpers/general.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/helpers/general.rs
@@ -1,8 +1,16 @@
-// use crate::models::general::llm::Message;
+#![allow(dead_code, unused_imports, unused_variables)]
+use crate::models::general::llm::Message;
 
 // appending a string to the and od an ai_function
-pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) { // -> Message
-    unimplemented!()
+pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
+    
+    let ai_function_str = ai_func(func_input);
+    dbg!(ai_function_str);
+     
+    Message {
+        role: "to be replaced later".to_string(),
+        content: "to be replaced later".to_string()
+    }
 }
 
 #[cfg(test)]
@@ -13,6 +21,8 @@ mod tests {
     // cargo test tests_extending_ai_function -- --nocapture
     #[test]
     fn tests_extending_ai_function() {
-        dbg!("tests_extending_ai_function");
+        // let x_str = convert_user_input_to_goal("dummy_variable");
+        // dbg!(x_str);
+        extend_ai_function(convert_user_input_to_goal, "dummy input you fool");
     }
 }
