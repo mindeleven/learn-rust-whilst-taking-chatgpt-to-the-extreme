6635049934273fae71644d1931e621ae0878817f
mindeleven <mindeleven@posteo.de>
2024-07-18 09:54:00 +0200

    Testing extending the string with giving instructions

--- sec08-auto-gpt-project/ch86_auto_gippity/src/helpers/general.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/helpers/general.rs
@@ -1,12 +1,24 @@
 #![allow(dead_code, unused_imports, unused_variables)]
+use std::fmt::format;
+
 use crate::models::general::llm::Message;
 
 // appending a string to the and od an ai_function
 pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
     
     let ai_function_str = ai_func(func_input);
-    dbg!(ai_function_str);
-     
+    // dbg!(ai_function_str);
+
+    // extend the string to encourage only printing the output
+    let msg: String = format!("FUNCTION: {}
+    INSTRUCTION: You are a function printer. You only print the results of functions.
+    Nothing else. No commentary. Here is the input to the function {}.
+    Print out what the function will return.",
+    ai_function_str, func_input);
+
+    dbg!(msg);
+    
+    // "Hopefully this is starting to make sense", Shaun just said
     Message {
         role: "to be replaced later".to_string(),
         content: "to be replaced later".to_string()
