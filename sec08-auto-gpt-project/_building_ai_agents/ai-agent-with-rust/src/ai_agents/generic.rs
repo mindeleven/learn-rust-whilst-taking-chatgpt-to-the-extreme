46d89f181a691f772eb3cfc48c00d03f8b17236b
mindeleven <mindeleven@posteo.de>
2024-07-18 14:50:20 +0200

    Filling the prompt method back in on the default Agent trait

--- sec08-auto-gpt-project/_building_ai_agents/ai-agent-with-rust/src/ai_agents/generic.rs
+++ sec08-auto-gpt-project/_building_ai_agents/ai-agent-with-rust/src/ai_agents/generic.rs
@@ -10,6 +10,11 @@
 use async_openai::{config::OpenAIConfig, Client as OpenAIClient};
 use crate::helpers::errors::ApiError;
 
+use async_openai::types::{
+    ChatCompletionRequestMessage, ChatCompletionRequestSystemMessageArgs,
+    ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs,
+};
+
 /// defining a generic agent interface
 /// the idea behind the generic interface is that it holds all of the relevant methods
 /// for the different agents
@@ -20,5 +25,46 @@ pub trait Agent {
     fn system_message(&self) -> String;
 
     // to be given a default implementation later
-    async fn prompt(&self, input: &str, data: String) -> Result<String, ApiError>;
+    // async fn prompt(&self, input: &str, data: String) -> Result<String, ApiError>;
+    async fn prompt(&self, input: &str, data: String) -> Result<String, ApiError> {
+        let input = format!(
+            "{input}
+
+            Provided context:
+            {}
+            ",
+            serde_json::to_string_pretty(&data)?
+        );
+        let res = self
+            .client()
+            .chat()
+            .create(
+                CreateChatCompletionRequestArgs::default()
+                    .model("gpt-4o")
+                    .messages(vec![
+                        //First we add the system message to define what the Agent does
+                        ChatCompletionRequestMessage::System(
+                            ChatCompletionRequestSystemMessageArgs::default()
+                                .content(&self.system_message())
+                                .build()?,
+                        ),
+                        //Then we add our prompt
+                        ChatCompletionRequestMessage::User(
+                            ChatCompletionRequestUserMessageArgs::default()
+                                .content(input)
+                                .build()?,
+                        ),
+                    ])
+                    .build()?,
+            )
+            .await
+            .map(|res| {
+                //We extract the first one
+                res.choices[0].message.content.clone().unwrap()
+            })?;
+
+        println!("Retrieved result from prompt: {res}");
+
+        Ok(res)
+    }
 }
