507c8927d6df672491663b43caeb72a3a6e194a0
mindeleven <mindeleven@posteo.de>
2024-07-20 10:40:45 +0200

    Writing out the functionality for a prompt webpage and adding it to the router

--- sec08-auto-gpt-project/_building_ai_agents/ai-agent-with-rust/src/main.rs
+++ sec08-auto-gpt-project/_building_ai_agents/ai-agent-with-rust/src/main.rs
@@ -1,10 +1,22 @@
 /// coding along with the tutorial `Building AI Agents with Rust` by Joshua Mo
 /// @ https://www.shuttle.rs/blog/2024/05/16/building-ai-content-writer-rust-gpt4o
-use axum::{routing::get, Router};
+use axum::{
+    extract::State,
+    Json,
+    response::IntoResponse,
+    routing::{get, post}, 
+    Router
+};
 use crate::ai_agents::{
     researcher::Researcher,
     writer::Writer
 };
+use serde::{
+    Deserialize, 
+    Serialize
+};
+use crate::helpers::errors::ApiError;
+use crate::ai_agents::generic::Agent;
 
 mod ai_agents;
 mod helpers;
@@ -28,8 +40,38 @@ impl AppState {
     }
 }
 
+/// writing the handler endpoint
+#[derive(Deserialize, Serialize)]
+pub struct Prompt {
+    q: String,
+}
 
+/* // -> fn prompt from tutorial
+#[axum::debug_handler]
+async fn prompt(
+    State(state): State<AppState>,
+    Json(prompt): Json<Prompt>,
+) -> Result<impl IntoResponse, ApiError> {
+    let data = state.researcher.prepare_data(&prompt.q).await?;
+    let resarcher_result = state.researcher.prompt(&prompt.q, data).await?;
+    let writer_result = state.writer.prompt(&prompt.q, res).await?;
 
+    Ok(writer_result)
+}
+*/
+
+// -> fn prompt from repo
+#[axum::debug_handler]
+async fn prompt(
+    State(state): State<AppState>,
+    Json(prompt): Json<Prompt>,
+) -> Result<impl IntoResponse, ApiError> {
+    let res = state.researcher.prepare_data(&prompt.q).await?;
+    let res = state.researcher.prompt(&prompt.q, res).await?;
+    let res = state.writer.prompt(&prompt.q, res).await?;
+
+    Ok(res)
+}
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -37,7 +79,12 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let state = AppState::new();
+
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/prompt", post(prompt))
+        .with_state(state);
 
     Ok(router.into())
 }
