0b2dc2f1bb0e644057955e44c752677fbfdad84d
mindeleven <mindeleven@posteo.de>
2024-07-22 12:47:41 +0200

    Implementing the basic traits for the basic agent

--- sec08-auto-gpt-project/ch86_auto_gippity/src/models/agent_basic/basic_agent.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/models/agent_basic/basic_agent.rs
@@ -1,4 +1,5 @@
 use crate::models::general::llm::Message;
+use crate::models::agent_basic::basic_traits::BasicTraits;
 
 #[derive(Debug, PartialEq)]
 pub enum AgentState {
@@ -14,4 +15,36 @@ pub struct BasicAgent {
     pub position: String,
     pub state: AgentState,
     pub memory: Vec<Message>, // storing conversation history
+}
+
+impl BasicTraits for BasicAgent {
+    fn new(objective: String, position: String) -> Self {
+        Self {
+            objective,
+            position,
+            state: AgentState::Discovery,
+            memory: Vec::from([])
+        
+        }
+    }
+
+    fn update_state(&mut self, new_state: AgentState) {
+        self.state = new_state;
+    }
+
+    fn get_objective(&self) -> &String {
+        &self.objective
+    }
+
+    fn get_position(&self) -> &String {
+        &self.position
+    }
+
+    fn get_state(&self) -> &AgentState {
+        &self.state
+    }
+
+    fn get_memory(&self) -> &Vec<Message> {
+        &self.memory
+    }
 }
