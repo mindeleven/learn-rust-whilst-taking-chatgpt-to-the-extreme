5b2b1993532ea766935103f8daaba9e46626ae50
mindeleven <mindeleven@posteo.de>
2024-07-23 11:14:17 +0200

    Added two more structs to represent the project scope and the api endpoints

--- sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents/agent_traits.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents/agent_traits.rs
@@ -7,11 +7,30 @@ use async_trait::async_trait;
 use serde::{ Deserialize, Serialize };
 use std::fmt::Debug;
 
+// how does the schema for the api endpoints that we expect look like?
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
+pub struct RouteObject {
+    is_route_dynamic: String,
+    method: String,
+    request_body: serde_json::Value,
+    route: String,
+}
+
+// the struct we expect the llm to return ??? (needs clarification)
+#[derive(Debug, Clone, Copy, Deserialize, Serialize, PartialEq)]
+pub struct ProjectScope {
+    is_crud_required: bool,
+    is_user_login_and_logout: bool,
+    is_external_urls_required: bool,
+}
+
 // building the fact sheet 
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub struct FactSheet {
     pub project_description: String,
-    pub project_scope: String,
+    // when we first create the project the scope will be None
+    // when we get the project it will be some
+    pub project_scope: Option<ProjectScope>,
     pub external_urls: String,
     pub backend_code: String,
     pub api_endpoint_schema: String,
