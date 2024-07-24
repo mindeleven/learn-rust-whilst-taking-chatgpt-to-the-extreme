f87e5138aa6d2c8b7854eca72353db165792c6ef
mindeleven <mindeleven@posteo.de>
2024-07-24 12:51:09 +0200

    Allowing the managing agent to pass the SpecialFunctions to the list of agents

--- sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents_manager/managing_agent.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents_manager/managing_agent.rs
@@ -1,10 +1,12 @@
 // managing agent will be responsible for managing agents with a special given trait
 use crate::models::agent_basic::basic_agent::{ AgentState, BasicAgent };
-use crate::models::agents::agent_traits::FactSheet;
+use crate::models::agents::agent_traits::{ FactSheet, SpecialFunctions };
 
 #[derive(Debug)]
 pub struct ManagingAgent {
     attributes: BasicAgent,
     factsheet: FactSheet,
-    agent: Vec<String> // vector of agents that have a particular trait
+    // giving agents the SpecialFunctions trait which will allow them 
+    // to use the functions defined there
+    agent: Vec<Box<dyn SpecialFunctions>> // vector of agents that have a particular trait
 }
