1c52014cc425bf533f4ee0cb895d7b4b78b9e233
mindeleven <mindeleven@posteo.de>
2024-07-26 12:22:56 +0200

    Added a few explanatory comments

--- sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents_manager/managing_agent.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents_manager/managing_agent.rs
@@ -4,9 +4,11 @@ use crate::models::agents::agent_traits::{ FactSheet, SpecialFunctions };
 
 #[derive(Debug)]
 pub struct ManagingAgent {
+    // our attributes will be of basic agent and all agents will have these attributes
     attributes: BasicAgent,
     factsheet: FactSheet,
     // giving agents the SpecialFunctions trait which will allow them 
     // to use the functions defined there
+    // each agent will have the ability to see the factsheet and update it accordingly
     agent: Vec<Box<dyn SpecialFunctions>> // vector of agents that have a particular trait
 }
