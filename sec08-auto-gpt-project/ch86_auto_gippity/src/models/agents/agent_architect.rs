2a5415e2bf795c36580718415e99856220c87c55
mindeleven <mindeleven@posteo.de>
2024-07-26 12:28:24 +0200

    Adding a few explanatory comments, bringing in some more imports

--- sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents/agent_architect.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents/agent_architect.rs
@@ -1,5 +1,8 @@
 use crate::ai_functions::aifunc_architect::{ print_project_scope, print_site_urls };
 use crate::helpers::command_line::PrintCommand;
+// bringing in the ability to go and request a llm, to give us information and then
+// decode this information into the struct or type we want
+// plus to check the status code of any urls
 use crate::helpers::general::{ ai_task_request_decoded, check_status_code };
 use crate::models::agent_basic::basic_agent::{ AgentState, BasicAgent };
 use crate::models::agent_basic::basic_traits::BasicTraits;
@@ -8,3 +11,9 @@ use crate::models::agents::agent_traits::{
     ProjectScope,
     SpecialFunctions
 };
+
+use async_trait::async_trait;
+use reqwest::Client;
+use std::time::Duration;
+
+// creating the Solutions Architect
