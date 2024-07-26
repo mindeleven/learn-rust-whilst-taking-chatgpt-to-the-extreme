0ebdac1e2f58a07fea457282407ab601fc04fa96
mindeleven <mindeleven@posteo.de>
2024-07-26 15:07:28 +0200

    Creating the Solutions Architect, adding an implementation with a constructor and functionality to retrieve the project scope

--- sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents/agent_architect.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents/agent_architect.rs
@@ -14,6 +14,44 @@ use crate::models::agents::agent_traits::{
 
 use async_trait::async_trait;
 use reqwest::Client;
+use std::fmt::format;
+use std::path::Prefix;
 use std::time::Duration;
 
 // creating the Solutions Architect
+#[derive(Debug)]
+struct AgentSolutionArchitect {
+    attributes: BasicAgent,
+}
+
+impl AgentSolutionArchitect {
+    fn new() -> Self {
+        let attributes = BasicAgent {
+            objective: "Gathers information and design solutions for website development".to_string(),
+            position: "Solutions Architect".to_string(),
+            state: AgentState::Discovery,
+            memory: vec![]
+        };
+
+        Self {
+            attributes
+        }
+    }
+
+    // retrieve project scope (which is done by the llm)
+    async fn call_project_scope(&mut self, factsheet: &mut FactSheet) -> ProjectScope {
+        let msg_context: String = format!("{}", factsheet.project_description);
+
+        let ai_response: ProjectScope = ai_task_request_decoded::<ProjectScope>(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_project_scope),
+            print_project_scope
+        ).await;
+
+        factsheet.project_scope = Some(ai_response.clone());
+        self.attributes.update_state(AgentState::Finished);
+
+        return ai_response;
+    }
+}
