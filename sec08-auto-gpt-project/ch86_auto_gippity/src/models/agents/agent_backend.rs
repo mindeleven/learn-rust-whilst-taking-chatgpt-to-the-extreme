ef42e2db48fccc239c8d4e941491b3986233eec1
mindeleven <mindeleven@posteo.de>
2024-07-29 15:16:12 +0200

    Structuring and implementing the AgentBackendDeveloper

--- sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents/agent_backend.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents/agent_backend.rs
@@ -4,17 +4,63 @@ use crate::ai_functions::aifunc_backend::{
     print_rest_api_endpoints,
 };
 use crate::helpers::general::{
-    check_status_code, read_code_template_contents, read_exec_main_contents, save_api_endpoints,
-    save_backend_code, WEB_SERVER_PROJECT_PATH,
+    check_status_code, read_code_template_contents, 
+    save_api_endpoints, save_backend_code
 };
 
-use crate::helpers::command_line::{confirm_safe_code, PrintCommand};
+use crate::helpers::command_line::PrintCommand;
 use crate::helpers::general::ai_task_request;
-use crate::models::agent_basic::basic_agent::{AgentState, BasicAgent};
-use crate::models::agents::agent_traits::{FactSheet, RouteObject, SpecialFunctions};
+use crate::models::agent_basic::basic_agent::{ AgentState, BasicAgent };
+use crate::models::agents::agent_traits::{ FactSheet, RouteObject, SpecialFunctions };
 
 use async_trait::async_trait;
 use reqwest::Client;
-use std::process::{Command, Stdio};
+use std::process::{ Command, Stdio };
 use std::time::Duration;
 use tokio::time;
+
+#[derive(Debug)]
+struct AgentBackendDeveloper {
+    attributes: BasicAgent,
+    bug_errors: Option<String>,
+    bug_count: u8
+}
+
+impl AgentBackendDeveloper {
+    pub fn new() -> Self {
+        let attributes = BasicAgent {
+            objective: "Gathers information and design solutions for website development".to_string(),
+            position: "Backend Developer".to_string(),
+            state: AgentState::Discovery,
+            memory: vec![]
+        };
+
+        Self {
+            attributes,
+            bug_errors: None,
+            bug_count: 0
+        }
+    }
+
+    async fn call_initial_backend_code(&self, factsheet: &mut FactSheet) {
+
+        let code_template_str: String = read_code_template_contents();
+
+        // concatenate instruction
+        let mut msg_context: String = format!(
+            "CODE TEMPLATE: {} \n PROJECT DESCRIPTION: {} \n",
+            code_template_str, factsheet.project_description
+        );
+
+        let ai_response = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_backend_webserver_code),
+            print_backend_webserver_code
+        ).await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
+
+    }
+}
