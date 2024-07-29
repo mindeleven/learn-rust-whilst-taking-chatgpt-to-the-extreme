a504df9f49e64837157a2609e7727124471634c4
mindeleven <mindeleven@posteo.de>
2024-07-29 09:32:43 +0200

    Implementing functionality for implementing a new ManagingAgent with a String as a parameter

--- sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents_manager/managing_agent.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/models/agents_manager/managing_agent.rs
@@ -2,6 +2,11 @@
 use crate::models::agent_basic::basic_agent::{ AgentState, BasicAgent };
 use crate::models::agents::agent_traits::{ FactSheet, SpecialFunctions };
 
+use crate::ai_functions::aifunc_managing::convert_user_input_to_goal;
+use crate::helpers::general::ai_task_request;
+use crate::models::agents::agent_architect::AgentSolutionArchitect;
+use crate::models::general::llm::Message;
+
 #[derive(Debug)]
 pub struct ManagingAgent {
     // our attributes will be of basic agent and all agents will have these attributes
@@ -10,5 +15,41 @@ pub struct ManagingAgent {
     // giving agents the SpecialFunctions trait which will allow them 
     // to use the functions defined there
     // each agent will have the ability to see the factsheet and update it accordingly
-    agent: Vec<Box<dyn SpecialFunctions>> // vector of agents that have a particular trait
+    agents: Vec<Box<dyn SpecialFunctions>> // vector of agents that have a particular trait
+}
+
+impl ManagingAgent {
+    pub async fn new(usr_req: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let position = "Project Manager".to_string();
+
+        let attributes = BasicAgent {
+            objective: "Manage agents who are building an excellent website for the user".to_string(),
+            position: position.clone(),
+            state: AgentState::Discovery,
+            memory: vec![]
+        };
+
+        let project_description: String = ai_task_request(
+            usr_req,
+            &position,
+            get_function_string!(convert_user_input_to_goal),
+            convert_user_input_to_goal
+        ).await;
+
+        let agents: Vec<Box<dyn SpecialFunctions>> = vec![];
+
+        let mut factsheet: FactSheet = FactSheet {
+            project_description,
+            project_scope: None,
+            external_urls: None,
+            backend_code: None,
+            api_endpoint_schema: None,
+        };
+
+        Ok(Self{
+            attributes,
+            factsheet,
+            agents
+        })
+    }
 }
