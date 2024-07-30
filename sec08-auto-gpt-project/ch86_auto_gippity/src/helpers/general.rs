9a3d787b8fb981749ded1cc8b5d9240d6de3fdbc
mindeleven <mindeleven@posteo.de>
2024-07-30 14:28:25 +0200

    Added helper function to get the contents of the main template

--- sec08-auto-gpt-project/ch86_auto_gippity/src/helpers/general.rs
+++ sec08-auto-gpt-project/ch86_auto_gippity/src/helpers/general.rs
@@ -118,6 +118,12 @@ pub fn read_code_template_contents() -> String {
     fs::read_to_string(CODE_TEMPLATE_PATH).expect("Unable to read code template")
 }
 
+// get exec main template
+pub fn read_exec_main_contents() -> String {
+    let path = String::from(EXEC_MAIN_PATH);
+    fs::read_to_string(CODE_TEMPLATE_PATH).expect("Unable to exec main template")
+}
+
 // (2) Save new Backend Code
 // once llm get's into write mode we'll need to safe the code created
 pub fn save_backend_code(contents: &String) {
