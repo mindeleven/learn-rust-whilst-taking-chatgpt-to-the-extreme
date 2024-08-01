47233b849b6cbb80194ef47603d2f01c43aa5363
mindeleven <mindeleven@posteo.de>
2024-08-01 09:47:23 +0200

    Webserver main.rs got modified with Rust code from llm

--- sec07-web-server-template/ch74_web_template/src/main.rs
+++ sec07-web-server-template/ch74_web_template/src/main.rs
@@ -1,115 +1,65 @@
-#![allow(unused_imports)]
-
-/// purpose of the project:
-/// building a web server that does a number of things
-/// as a support for a to do app
-/// it can create, read, update and delete data 
-/// or fetch data from somewhere
-/// 
-/// json example of a task:
-/// http://localhost:8080/task
-/*
-{
-    "id": 1,
-    "name": "my first task",
-    "completed": false
-}
-*/
-/// json example of a user:
-/// http://localhost:8080/register
-/*
-{
-    "id": 1,
-    "username": "joe_doe",
-    "password": "123password"
-}
-*/
-
 use actix_cors::Cors;
-
-use actix_web::web::post;
-use actix_web::{
-    http::header, 
-    web, 
-    App, 
-    HttpResponse, 
-    HttpServer, 
-    Responder
-};
-
-use serde::{
-    Serialize,
-    Deserialize
-};
-
+use actix_web::{http::header, web, App, HttpServer, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
 use reqwest::Client as HttpClient;
-
 use async_trait::async_trait;
-
 use std::sync::Mutex;
 use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
 
-/// server is going to represent simple to do app
-/// structuring data we're going to store in a json file
-/// storing task data
 #[derive(Serialize, Deserialize, Debug, Clone)]
-struct Task {
+struct FitnessProgress {
     id: u64,
-    name: String,
-    completed: bool,
+    user_id: u64,
+    date: String,
+    progress: String,
+    timezone: String,
 }
-/// storing user data
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct User {
     id: u64,
     username: String,
-    password: String, // easy storing as a string here
+    password: String,
 }
-/// database
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Database {
-    tasks: HashMap<u64, Task>,
+    progress: HashMap<u64, FitnessProgress>,
     users: HashMap<u64, User>,
 }
 
-// implementing the database functionality
 impl Database {
-    // creating new database
     fn new() -> Self {
         Self {
-            tasks: HashMap::new(),
+            progress: HashMap::new(),
             users: HashMap::new(),
         }
     }
 
-    // CRUD DATA RELATED FUNCTIONS
-    fn insert(&mut self, task: Task) {
-        self.tasks.insert(task.id, task);
-    }
-    
-    fn get(&self, id: &u64) -> Option<&Task> {
-        self.tasks.get(id)
+    // CRUD for FitnessProgress
+    fn insert_progress(&mut self, progress: FitnessProgress) {
+        self.progress.insert(progress.id, progress);
     }
 
-    fn get_all(&self) -> Vec<&Task> {
-        self.tasks.values().collect()
-    }
-    
-    fn update(&mut self, task: Task) {
-        self.tasks.insert(task.id, task);
+    fn get_progress(&self, id: &u64) -> Option<&FitnessProgress> {
+        self.progress.get(id)
     }
 
-    fn delete(&mut self, id: &u64) {
-        self.tasks.remove(id);
+    fn get_all_progress(&self) -> Vec<&FitnessProgress> {
+        self.progress.values().collect()
     }
-   
-    // fn update(&mut self, user: User) {
-    //     self.users.insert(user.id, user);
-    // }
 
-    // USER DATA RELATED FUNCTIONS
+    fn delete_progress(&mut self, id: &u64) {
+        self.progress.remove(id);
+    }
+
+    fn update_progress(&mut self, progress: FitnessProgress) {
+        self.progress.insert(progress.id, progress);
+    }
+
+    // CRUD for User
     fn insert_user(&mut self, user: User) {
         self.users.insert(user.id, user);
     }
@@ -119,140 +69,133 @@ impl Database {
     }
 
     // DATABASE SAVING
-    // converting the hashmap into a using file
     fn save_to_file(&self) -> std::io::Result<()> {
-        let data = serde_json::to_string(&self)?;
-        let mut file: fs::File = fs::File::create("./data/database.json")?;
+        let data: String = serde_json::to_string(&self)?;
+        let mut file: fs::File = fs::File::create("database.json")?;
         file.write_all(data.as_bytes())?;
         Ok(())
     }
-    
-    fn load_from_file() -> std::io::Result<Self> { // Self is type Database
-        let file_content = fs::read_to_string("./data/database.json")?;
+
+    fn load_from_file() -> std::io::Result<Self> {
+        let file_content: String = fs::read_to_string("database.json")?;
         let db: Database = serde_json::from_str(&file_content)?;
         Ok(db)
     }
-
 }
 
-// we've shared data that users will try to access at the same time
-// therefore we implement Mutex
-struct AppState { // state of the application
-    // database data will be shared so we wrap it into a Mutex
+struct AppState {
     db: Mutex<Database>,
 }
 
-// call to webserver
-// returns responder from actix web
-// passing in the app state and data (the dask) in Json format
-// the type returned will implement the Responder trait
-async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
-    // db is wrapped in Mutex so we need to lock it
-    let mut db = app_state.db.lock().unwrap();
-    // creating a task
-    // into_inner is extracting the task out of what it's wrapped into
-    db.insert(task.into_inner());
+async fn create_progress(app_state: web::Data<AppState>, progress: web::Json<FitnessProgress>) -> impl Responder {
+    let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
+    db.insert_progress(progress.into_inner());
     let _ = db.save_to_file();
     HttpResponse::Ok().finish()
 }
 
-// async function to read a task
-async fn read_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
-    let db = app_state.db.lock().unwrap();
-    // gettingbthe u64 out of path and matching it
-    match db.get(&id.into_inner()) {
-        Some(task) => HttpResponse::Ok().json(task),
-        None => HttpResponse::NotFound().finish()
+async fn read_progress(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
+    let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
+    match db.get_progress(&id.into_inner()) {
+        Some(progress) => HttpResponse::Ok().json(progress),
+        None => HttpResponse::NotFound().finish(),
     }
 }
 
-// async function to read all tasks
-async fn read_all_tasks(app_state: web::Data<AppState>) -> impl Responder {
-    let db = app_state.db.lock().unwrap();
-    let tasks = db.get_all();
-    HttpResponse::Ok().json(tasks)
+async fn read_all_progress(app_state: web::Data<AppState>) -> impl Responder {
+    let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
+    let progress = db.get_all_progress();
+    HttpResponse::Ok().json(progress)
 }
 
-// async function to update a task
-async fn update_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
-    let mut db = app_state.db.lock().unwrap();
-    db.update(task.into_inner());
+async fn update_progress(app_state: web::Data<AppState>, progress: web::Json<FitnessProgress>) -> impl Responder {
+    let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
+    db.update_progress(progress.into_inner());
     let _ = db.save_to_file();
     HttpResponse::Ok().finish()
 }
 
-// async function to delete a task
-async fn delete_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
-    let mut db = app_state.db.lock().unwrap();
-    db.delete(&id.into_inner());
+async fn delete_progress(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
+    let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
+    db.delete_progress(&id.into_inner());
     let _ = db.save_to_file();
     HttpResponse::Ok().finish()
 }
 
-// USER functionality
-// async function to register a user
 async fn register(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
-    let mut db = app_state.db.lock().unwrap();
+    let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     db.insert_user(user.into_inner());
     let _ = db.save_to_file();
     HttpResponse::Ok().finish()
 }
 
-// async function to log in a user
 async fn login(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
-    let db = app_state.db.lock().unwrap();
+    let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     match db.get_user_by_name(&user.username) {
-        Some(stored_user) if stored_user.password == user.password => {
-            HttpResponse::Ok().body("You're logged in!")
-        },
-        _ => HttpResponse::BadRequest().body("Invalid username or password")
+        Some(stored_user) if stored_user.password == user.password => HttpResponse::Ok().body("Logged in!"),
+        _ => HttpResponse::BadRequest().body("Invalid username or password"),
+    }
+}
+
+#[async_trait]
+trait ExternalApi {
+    async fn fetch_timezone(&self, timezone: &str) -> Result<String, reqwest::Error>;
+    async fn fetch_exercise_info(&self, exercise_id: u64) -> Result<String, reqwest::Error>;
+}
+
+struct ApiClient {
+    client: HttpClient,
+}
+
+#[async_trait]
+impl ExternalApi for ApiClient {
+    async fn fetch_timezone(&self, timezone: &str) -> Result<String, reqwest::Error> {
+        let url = format!("https://worldtimeapi.org/api/timezone/{}", timezone);
+        let response = self.client.get(&url).send().await?.text().await?;
+        Ok(response)
+    }
+
+    async fn fetch_exercise_info(&self, exercise_id: u64) -> Result<String, reqwest::Error> {
+        let url = format!("https://wger.de/api/v2/exerciseinfo/{}", exercise_id);
+        let response = self.client.get(&url).send().await?.text().await?;
+        Ok(response)
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // load the database or create a new one
-    let db = match Database::load_from_file() {
+    let db: Database = match Database::load_from_file() {
         Ok(db) => db,
-        Err(_) => Database::new()
-    }; // what we get back here is a Database
+        Err(_) => Database::new(),
+    };
 
-    let data = web::Data::new(AppState {
-        // wrapping our Database into a Mutex so we can share it on multiple threads
-        db: Mutex::new(db), // wrapping our Database into a Mutex, see definition above
+    let data: web::Data<AppState> = web::Data::new(AppState {
+        db: Mutex::new(db),
     });
-    
-    // creating new server
+
     HttpServer::new(move || {
-        // creating new App
         App::new()
-            // cross origin resource sharing
-            // == making calls to our web server from any port or domain
-            // handling calls with wrap()
             .wrap(
                 Cors::permissive()
                     .allowed_origin_fn(|origin, _req_head| {
-                        origin.as_bytes().starts_with(b"http://localhost") 
-                            || origin == "null"
-
+                        origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
                     })
                     .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-                    .allowed_headers(vec![header::AUTHORIZATION, header:: ACCEPT])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
                     .supports_credentials()
-                    .max_age(3600)
+                    .max_age(3600),
             )
-            .app_data(data.clone()) // is not cloning as a deep copy bc web::Data is a smart pointer
-            .route("/task", web::post().to(create_task)) // to means what function to run
-            .route("/task/{id}", web::get().to(read_task))
-            .route("/task", web::get().to(read_all_tasks))
-            .route("/task", web::put().to(update_task))
-            .route("/task/{id}", web::delete().to(delete_task))
+            .app_data(data.clone())
+            .route("/progress", web::post().to(create_progress))
+            .route("/progress", web::get().to(read_all_progress))
+            .route("/progress", web::put().to(update_progress))
+            .route("/progress/{id}", web::get().to(read_progress))
+            .route("/progress/{id}", web::delete().to(delete_progress))
             .route("/register", web::post().to(register))
             .route("/login", web::post().to(login))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
-        
 }
